74b126e89a81a607dead6662a7e7e5d2e9abec61
Matthew <Matthew_19@users.noreply.github.com>
2020-01-30 00:56:31 +0100

    asd

--- ProjektLabCpp/ProjektLabCpp/Kierownik.cpp
+++ ProjektLabCpp/ProjektLabCpp/Kierownik.cpp
@@ -1,20 +1,24 @@
 #include "Kierownik.h"
 
-Kierownik::Kierownik(int m_nLiczbaPracownikow = 0, Napis m_NazwaDzialu = "") {
-
-}
-Kierownik::Kierownik(Kierownik& k) {
-
-}
+Kierownik::Kierownik(int m_nLiczbaPracownikow = 0, Napis m_NazwaDzialu = "") 
+	:m_nLiczbaPracownikow(m_nLiczbaPracownikow), m_NazwaDzialu(m_NazwaDzialu)
+{}
+Kierownik::Kierownik(Kierownik& k) 
+	:m_nLiczbaPracownikow(k.m_nLiczbaPracownikow), m_NazwaDzialu(k.m_NazwaDzialu)
+{}
 Kierownik& Kierownik::operator= (Kierownik& k) {
-
+	if (this == &k)return *this;
+	this->~Kierownik();
+	new(this) Kierownik(k);
+	return *this;
 }
 bool Kierownik::operator== (const Kierownik& k) const {
-
+	return k == *this and k.m_nLiczbaPracownikow == m_nLiczbaPracownikow and !k.m_NazwaDzialu.SprawdzNapis(this->m_NazwaDzialu.Zwroc());
 }
 void Kierownik::WypiszDane() {
-
+	Pracownik::WypiszDane();
+	cout << " " << m_NazwaDzialu << " L. prac: " << m_nLiczbaPracownikow;
 }
 Pracownik* Kierownik::KopiaObiektu() {
-
+	return new Kierownik(*this);
 }
